00cd9af84ad2bfbf2bcb0241a1160f13e8513460
AliRizaAynaci <aynacialiriza@gmail.com>
2025-10-05 05:17:51 +0300

    change the redirect url

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,4 @@
-const DEFAULT_API_BASE_URL = 'https://clean-breathing-front-six.vercel.app/';
+const DEFAULT_API_BASE_URL = 'https://clean-breathing-710737072c4d.herokuapp.com';
+
 
 export const getApiBaseUrl = () => process.env.REACT_APP_API_BASE_URL || DEFAULT_API_BASE_URL;
